9846638e8526c5333a7ab2140d2eaa421216e306
Oleg Smirnov <sm-oleg1209@yandex.ru>
2020-07-24 11:40:46 +0700

    Replace generator with list to speed up pattern search

--- research/django.py
+++ research/django.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from typing import List, Generator
+from typing import List
 
 import pandas as pd
 from tqdm import tqdm
@@ -14,16 +14,17 @@ from preprocessing.loaders import MinerOutputLoader
 from vcs.traverse import GitAnalyzer, Method
 
 
-def traverse_python_methods(repository_root: str) -> Generator[Method, None, None]:
+def get_all_python_methods(repository_root: str) -> List[Method]:
+    methods = []
     for root, dirs, files in os.walk(repository_root):
         for file in files:
             if file.endswith('.py'):
                 path_to_python_file = os.path.join(root, file)
                 with open(path_to_python_file, 'r') as f:
                     src = f.read()
-                methods: List[Method] = GitAnalyzer._extract_methods(path_to_python_file, src)
-                for method in methods:
-                    yield method
+                current_methods: List[Method] = GitAnalyzer._extract_methods(path_to_python_file, src)
+                methods.extend(current_methods)
+    return methods
 
 
 if __name__ == '__main__':
@@ -42,7 +43,8 @@ if __name__ == '__main__':
         pattern_graph_by_path[pattern_graph_path] = load_nx_graph_from_dot_file(pattern_graph_path)
 
     results = {}
-    for method in traverse_python_methods(repository_root=os.path.join(config.REPOSITORIES_ROOT, 'django')):
+    all_methods = get_all_python_methods(os.path.join(config.REPOSITORIES_ROOT, 'django'))
+    for method in tqdm(all_methods):
         pfg = pyflowgraph.build_from_source(method.get_source())
         method_graph = load_nx_graph_from_pyflowgraph(pfg)
         seeker = SubgraphSeeker(method_graph)
