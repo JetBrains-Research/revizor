ce831022e75a0a7c6160dc79b0561d2478565c18
SmirnovOleg <sm-oleg1209@yandex.ru>
2020-07-31 10:39:28 +0300

    Move JTextField creation to separate method & Fix static config

--- bug-finder-plugin/src/main/kotlin/org/jetbrains/research/BugFinderInspection.kt
+++ bug-finder-plugin/src/main/kotlin/org/jetbrains/research/BugFinderInspection.kt
@@ -4,52 +4,66 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.ui.DocumentAdapter
+import com.jetbrains.python.psi.PyBinaryExpression
 import com.jetbrains.python.psi.PyElementVisitor
+import java.nio.file.Path
+import java.nio.file.Paths
 import javax.swing.*
 import javax.swing.event.DocumentEvent
+import kotlin.reflect.KMutableProperty0
 
 class BugFinderInspection : LocalInspectionTool() {
-    private var patternsOutputPath = "/home/oleg/prog/jetbrains/data/relaunch_output"
-    private var codeChangeMinerPath = "/home/oleg/prog/jetbrains/"
+    companion object {
+        var patternsOutputPath: Path = Paths.get("/home/oleg/prog/jetbrains/data/relaunch_output")
+        var codeChangeMinerPath: Path = Paths.get("/home/oleg/prog/jetbrains/bug-finder/code_change-miner")
+        var pythonExecutablePath: Path = Paths.get("/home/oleg/miniconda3/envs/bug-finder/bin/python")
+    }
 
     override fun createOptionsPanel(): JComponent? {
         val panel = JPanel()
         panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
 
-        val labelEnterPatternsOutputPath = JLabel("Enter path to code-change-miner output directory:")
-        val textFieldPatternsOutputPath = JTextField(patternsOutputPath, 60).also {
-            it.maximumSize = it.preferredSize
-        }
-        textFieldPatternsOutputPath.document.addDocumentListener(object : DocumentAdapter() {
-            override fun textChanged(event: DocumentEvent) {
-                patternsOutputPath = textFieldPatternsOutputPath.text
+        fun createTextFieldBoundToPathProperty(pathProperty: KMutableProperty0<Path>): JTextField {
+            val textField = JTextField(pathProperty.get().toString(), 60).also {
+                it.maximumSize = it.preferredSize
             }
-        })
+            textField.document.addDocumentListener(object : DocumentAdapter() {
+                override fun textChanged(event: DocumentEvent) {
+                    pathProperty.set(Paths.get(textField.text))
+                }
+            })
+            return textField
+        }
+
+        val labelEnterPatternsOutputPath = JLabel("Enter path to code-change-miner output directory:")
+        val textFieldPatternsOutputPath = createTextFieldBoundToPathProperty(Companion::patternsOutputPath)
 
         val labelCodeChangeMinerPath = JLabel("Enter path to code-change-miner package:")
-        val textFieldCodeChangeMinerPath = JTextField(codeChangeMinerPath, 60).also {
-            it.maximumSize = it.preferredSize
-        }
-        textFieldCodeChangeMinerPath.document.addDocumentListener(object : DocumentAdapter() {
-            override fun textChanged(event: DocumentEvent) {
-                codeChangeMinerPath = textFieldCodeChangeMinerPath.text
-            }
-        })
+        val textFieldCodeChangeMinerPath = createTextFieldBoundToPathProperty(Companion::codeChangeMinerPath)
+
+        val labelPythonExecutablePath = JLabel("Enter path to python executable:")
+        val textFieldPythonExecutablePath = createTextFieldBoundToPathProperty(Companion::pythonExecutablePath)
 
         with(panel) {
-            add(Box.createVerticalGlue())
             add(labelEnterPatternsOutputPath)
             add(textFieldPatternsOutputPath)
-            add(Box.createVerticalGlue())
             add(labelCodeChangeMinerPath)
             add(textFieldCodeChangeMinerPath)
-            add(Box.createVerticalGlue())
+            add(labelPythonExecutablePath)
+            add(textFieldPythonExecutablePath)
         }
         return panel
     }
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-        return object : PyElementVisitor() {}
+        return object : PyElementVisitor() {
+            override fun visitPyBinaryExpression(node: PyBinaryExpression?) {
+                super.visitPyBinaryExpression(node)
+                if (node != null) {
+                    holder.registerProblem(node.originalElement, "ALERT")
+                }
+            }
+        }
     }
 
 }
