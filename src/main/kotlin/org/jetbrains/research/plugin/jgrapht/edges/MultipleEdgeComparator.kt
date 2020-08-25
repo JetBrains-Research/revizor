d829a05967c565c5e10cad481145982c1055dac2
SmirnovOleg <sm-oleg1209@yandex.ru>
2020-08-25 12:09:38 +0300

    Fix bug in MultipleEdgeComparator for nested edges

--- src/main/kotlin/org/jetbrains/research/plugin/jgrapht/edges/MultipleEdgeComparator.kt
+++ src/main/kotlin/org/jetbrains/research/plugin/jgrapht/edges/MultipleEdgeComparator.kt
@@ -2,13 +2,13 @@ package org.jetbrains.research.plugin.jgrapht.edges
 
 class MultipleEdgeComparator : Comparator<PatternSpecificMultipleEdge> {
 
-    private val edgeComparator: Comparator<PatternSpecificEdge> =
-        compareBy({ it.xlabel }, { it.fromClosure })
+    private val edgeComparator: Comparator<PatternSpecificEdge?> =
+        compareBy({ it?.xlabel }, { it?.fromClosure })
 
     override fun compare(fromTarget: PatternSpecificMultipleEdge?, fromPattern: PatternSpecificMultipleEdge?): Int {
         if (fromTarget?.embeddedEdgeByXlabel == null || fromPattern?.embeddedEdgeByXlabel == null)
             return 1
-        for (xlabel in fromTarget.embeddedEdgeByXlabel.keys) {
+        for (xlabel in fromPattern.embeddedEdgeByXlabel.keys) {
             val comparisonResult = edgeComparator.compare(
                 fromTarget.embeddedEdgeByXlabel[xlabel],
                 fromPattern.embeddedEdgeByXlabel[xlabel]
