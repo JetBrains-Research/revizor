219d35d007716bd176ce45a8d7b584cca6fdcdfe
SmirnovOleg <sm-oleg1209@yandex.ru>
2020-08-25 09:48:04 +0300

    Make EdgeComparators more explicit

---
+++ src/main/kotlin/org/jetbrains/research/plugin/jgrapht/edges/MultipleEdgeComparator.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.research.plugin.jgrapht.edges
+
+class MultipleEdgeComparator : Comparator<PatternSpecificMultipleEdge> {
+
+    private val edgeComparator: Comparator<PatternSpecificEdge> =
+        compareBy({ it.xlabel }, { it.fromClosure })
+
+    override fun compare(fromTarget: PatternSpecificMultipleEdge?, fromPattern: PatternSpecificMultipleEdge?): Int {
+        if (fromTarget?.embeddedEdgeByXlabel == null || fromPattern?.embeddedEdgeByXlabel == null)
+            return 1
+        for (xlabel in fromTarget.embeddedEdgeByXlabel.keys) {
+            val comparisonResult = edgeComparator.compare(
+                fromTarget.embeddedEdgeByXlabel[xlabel],
+                fromPattern.embeddedEdgeByXlabel[xlabel]
+            )
+            if (comparisonResult != 0) return comparisonResult else continue
+        }
+        return 0
+    }
+}
