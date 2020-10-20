23a8e573ac75e6e54ce742d9cae642e0741e136a
Oleg Smirnov <sm-oleg1209@yandex.ru>
2020-10-20 16:49:23 +0300

    Temporary fix for var-usage and var-decl

--- src/main/kotlin/org/jetbrains/research/plugin/jgrapht/vertices/WeakVertexComparator.kt
+++ src/main/kotlin/org/jetbrains/research/plugin/jgrapht/vertices/WeakVertexComparator.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.research.plugin.jgrapht.vertices
 
 import org.jetbrains.research.plugin.jgrapht.vertices.PatternSpecificVertex.LabelsGroup.Indicator.*
+import org.jetbrains.research.plugin.pyflowgraph.models.DataNode
 
 /**
  * A vertices comparator needed for the JGraphT `VF2SubgraphIsomorphismInspector`.
@@ -11,8 +12,15 @@ class WeakVertexComparator : Comparator<PatternSpecificVertex> {
 
         // Smart check for the variable names correspondence
         if (fromTarget?.label?.startsWith("var") == true
-            && fromPattern?.label?.startsWith("var") == true
+                && fromPattern?.label?.startsWith("var") == true
         ) {
+            // FIXME: Differ <variable-usage> and <variable-decl> nodes in the original miner
+            // Now it is temporary solution
+            val patternVarRoleLabel = (fromPattern.origin as? DataNode?)?.kind
+            val targetVarRoleLabel = (fromTarget.origin as? DataNode?)?.kind
+            if (patternVarRoleLabel != null && targetVarRoleLabel != null && patternVarRoleLabel != targetVarRoleLabel) {
+                return 1
+            }
             return when (fromPattern.dataNodeInfo?.whatMatters) {
                 VALUABLE_ORIGINAL_LABEL ->
                     if (fromPattern.dataNodeInfo!!.labels.contains(fromTarget.originalLabel)) 0 else 1
@@ -24,14 +32,14 @@ class WeakVertexComparator : Comparator<PatternSpecificVertex> {
 
         // PySubscriptionExpressions always match
         if (fromPattern?.originalLabel?.matches("""^.*?\[.*?\]$""".toRegex()) == true
-            && fromTarget?.originalLabel?.matches("""^.*?\[.*?\]$""".toRegex()) == true
+                && fromTarget?.originalLabel?.matches("""^.*?\[.*?\]$""".toRegex()) == true
         ) {
             return 0
         }
 
         // Otherwise check only labels and original labels
         if (fromTarget?.originalLabel?.toLowerCase() == fromPattern?.originalLabel?.toLowerCase()
-            && fromTarget?.label?.toLowerCase() == fromPattern?.label?.toLowerCase()
+                && fromTarget?.label?.toLowerCase() == fromPattern?.label?.toLowerCase()
         ) {
             return 0
         }
